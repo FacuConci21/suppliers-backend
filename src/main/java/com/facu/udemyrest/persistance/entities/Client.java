ea54a71e3cf482084a3c201cd17fa98214ffc6eb
Facu Conci <facu@example.com>
2021-08-04 16:03:19 -0300

    All working

---
+++ src/main/java/com/facu/udemyrest/persistance/entities/Client.java
@@ -0,0 +1,43 @@
+package com.facu.udemyrest.persistance.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity @Table(name = "clientes")
+public class Client implements Serializable {
+
+    @Id @Column(name = "id_cli")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCli;
+
+    @Column(name = "razon_social", nullable = true, length = 100)
+    private String razonSocial;
+
+    @Column(name = "fecha_activ", nullable = true, length = 100)
+    private String fechaActiv;
+
+    public Long getIdCli() {
+        return this.idCli;
+    }
+
+    public void setIdCli(Long idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getRazonSocial() {
+        return this.razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getFechaActiv() {
+        return this.fechaActiv;
+    }
+
+    public void setFechaActiv(String fechaActiv) {
+        this.fechaActiv = fechaActiv;
+    }
+}
