d3fa0e99b36ca8813a6287e61c8dfbb939afbf92
Facu Conci <facu@example.com>
2021-08-04 15:02:30 -0300

    Initial commit

---
+++ src/main/java/com/facu/udemyrest/UdemyrestApplication.java
@@ -0,0 +1,13 @@
+package com.facu.udemyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facu/udemyrest/UdemyrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facu.udemyrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
