987b7c8ccdeeb08491a39be7f99e10974fe99ea6
Facu Conci <facu@example.com>
2021-09-01 10:51:54 -0300

    savepoint

--- src/main/java/com/facu/udemyrest/irest/ApiController.java
+++ src/main/java/com/facu/udemyrest/irest/ApiController.java
@@ -1,11 +1,11 @@
 package com.facu.udemyrest.irest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ApiController<T, ID> {
 
@@ -13,10 +13,10 @@ public interface ApiController<T, ID> {
     List<T> getAll();
 
     @GetMapping()
-    Optional<T> getById(ID id);
+    ResponseEntity<?> getById(ID id);
 
     @PostMapping()
-    T post(T t);
+    ResponseEntity<?> post(T t);
 
     @DeleteMapping()
     void delete(ID id);
