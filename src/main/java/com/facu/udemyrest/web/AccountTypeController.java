7bf5fc60eaee0bc2abd3d0423aafcbbfe7f4d822
Facu Conci <facu@example.com>
2021-09-02 18:01:15 -0300

    Added CORS policy to Location and AccountType controllers.

--- src/main/java/com/facu/udemyrest/web/AccountTypeController.java
+++ src/main/java/com/facu/udemyrest/web/AccountTypeController.java
@@ -3,13 +3,12 @@ package com.facu.udemyrest.web;
 import com.facu.udemyrest.domain.service.AccountTypeService;
 import com.facu.udemyrest.persistance.entities.AccountType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200/" }, methods = RequestMethod.GET)
 @RequestMapping("/api/account-type")
 public class AccountTypeController {
 
--- src/main/java/com/facu/udemyrest/web/LocationController.java
+++ src/main/java/com/facu/udemyrest/web/LocationController.java
@@ -3,13 +3,12 @@ package com.facu.udemyrest.web;
 import com.facu.udemyrest.domain.service.LocationService;
 import com.facu.udemyrest.persistance.entities.Location;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200/" }, methods = RequestMethod.GET)
 @RequestMapping(value = "/api/location")
 public class LocationController {
 
